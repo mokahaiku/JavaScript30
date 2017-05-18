086ee644bfbbbfa7e6b32440f1a95a393d516ea6
Dovydas <dovydas@ceilutka.lt>
2017-05-18 07:28:46 +0300

    Solved first exercise

---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,21 @@
+const playSound = keyCode => {
+  const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
+  if (!sound) return 0;
+  sound.currentTime = 0;
+  sound.play();
+  return sound.duration * 1000;
+}
+
+const highlightBox = (keyCode, soundDuration) => {
+  const box = document.querySelector(`.key[data-key="${keyCode}"]`);
+  if (!box) return false;
+  box.classList.add('playing');
+  setTimeout(() => box.classList.remove('playing'), soundDuration);
+}
+
+(function() {
+  window.addEventListener('keydown', event => {
+    const soundDuration = playSound(event.keyCode);
+    highlightBox(event.keyCode, soundDuration);
+  });
+})();
